e7c70aaaaf3f5c59d2a34b763780bd482c767aca
tgjeon <skfkstkfka7>
2016-08-05 16:14:04 +0900

    Write comments in Korean Add YouTube link

--- 04-Date.py
+++ 04-Date.py
@@ -1,5 +1,5 @@
 """
-04. 날짜를 사용해봅시다. (38분)
+04. 날짜를 사용해봅시다. (분)
 
 - Original version (MVA): https://mva.microsoft.com/ko/training-courses/python%EC%9D%84-%EC%82%AC%EC%9A%A9%ED%95%9C-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D-%EC%86%8C%EA%B0%9C-8360
 - Compact version (Youtube): https://youtu.be/emY34tSKXc4?t=1h17m24s
