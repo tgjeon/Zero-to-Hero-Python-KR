e6d0c46912d33d3efb4b07abd7a823ac08b0a327
tgjeon <tgjeon@gist.ac.kr>
2016-08-07 07:53:24 +0900

    minor change

--- 02-Variable.py
+++ 02-Variable.py
@@ -1,7 +1,7 @@
 # -*- coding: utf8 -*-
 
 """
-02. 변수를 사용해봅시다. (19분)
+02. 변수를 사용해봅시다. (소요시간: 19분 강의)
 
 - Original version (MVA): https://mva.microsoft.com/ko/training-courses/python%EC%9D%84-%EC%82%AC%EC%9A%A9%ED%95%9C-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D-%EC%86%8C%EA%B0%9C-8360
 - Compact version (Youtube): https://youtu.be/emY34tSKXc4?t=20m1s
