968d4ffbf8fd87001d7a5d0af28c10cf0621459e
tgjeon <tgjeon@gist.ac.kr>
2016-08-18 22:21:30 +0900

    While Loop events

---
+++ 08-WhileLoop.py
@@ -0,0 +1,19 @@
+# -*- coding: utf8 -*-
+
+"""
+08. 완료될 때 까지 이벤트 반복. (소요시간: 분 강의)
+
+- Original version (MVA): https://mva.microsoft.com/ko/training-courses/python%EC%9D%84-%EC%82%AC%EC%9A%A9%ED%95%9C-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D-%EC%86%8C%EA%B0%9C-8360
+- Compact version (Youtube): https://youtu.be/emY34tSKXc4?t=2h35m10s
+- 참고:
+- 아래 설명은 Compact version 영상을 기준으로 작성되었습니다.
+"""
+
+"""
+완료될 때 까지 이벤트를 반복해봅시다.
+"""
+answer = '0'
+
+while answer != '4':
+    answer = input('What is 2 + 2 ? ')
+
