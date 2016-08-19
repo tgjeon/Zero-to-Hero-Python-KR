fd983d5eb9d4986e6467a439e7793f283df1665b
tgjeon <tgjeon@gist.ac.kr>
2016-08-19 14:49:58 +0900

    While Loop events

--- 08-WhileLoop.py
+++ 08-WhileLoop.py
@@ -17,3 +17,40 @@ answer = '0'
 while answer != '4':
     answer = input('What is 2 + 2 ? ')
 
+# while 문 이후 조건이 참인 경우 계속 반복합니다.
+# 위에서는 "answer가 4가 아닌 것"이 (참) 인 경우, 계속 반복 수행합니다
+
+
+"""
+거북이를 다시 소환해봅시다.
+"""
+import turtle
+counter = 0
+while counter < 4:
+    turtle.forward(100)
+    turtle.right(90)
+    counter = counter+1
+
+# counter를 이용하여 이전에 설명한 아래의 for loop와 동일하게 동작하도록 할 수 있습니다.
+#
+# import turtle
+# for step in range(4):
+#     turtle.forward(100)
+#     turtle.right(90)
+
+# for와 while을 이용한 두 반복문 모두 동일하게 동작합니다.
+# 개인의 기호에 따라 쓰세요
+# 하지만, while의 경우 condition을 직접 다루어야 하기 때문에, 특히 인덱스에 조심해서 사용해야합니다
+# 조건문에 들어가는 counter를 변화시키는 것을 꼭! 목적에 맞게 바뀌고 있는지 확인해야 합니다.
+
+# 참고자료: Code Complete - Steve McConnell 책을 읽어보시는 걸 추천드립니다.
+
+
+import turtle
+counter = 0
+while counter < 4:
+    turtle.forward(100)
+    turtle.right(90)
+    counter += 1
+
+# counter += 1 은 counter = counter + 1 과 동일하게 동작합니다
